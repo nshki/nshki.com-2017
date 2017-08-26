86695a0ecfe06ee43903cf7b230c78b7017e7b0b
Nishiki Liu <nshki@users.noreply.github.com>
2017-08-26 16:01:46 -0400

    Handle swipe detection using Hammer.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -158,13 +158,10 @@
   /*
    * Enable swipe control using moveScene().
    */
-  swipeDetect($('#swipe-area'), function(dir) {
-    if (dir === 'up') {
-      moveScene('forward');
-    } else if (dir === 'down') {
-      moveScene('back');
-    }
-  });
+  var hammer = new Hammer.Manager(document.body);
+  hammer.add(new Hammer.Swipe());
+  hammer.on('swipeup', function() { moveScene('forward') });
+  hammer.on('swipedown', function() { moveScene('back') });
 
   /*
    * Enable keyboard control using moveScene().
@@ -281,54 +278,5 @@
     TweenLite.set($('#rocket'), { opacity: 0, rotation: 10, left: '90%', top: '100%' });
     TweenLite.set($('#backdrop-moon'), { opacity: 0, y: 200 });
     TweenLite.set($('#space-cta'), { opacity: 0, scale: 0 });
-  }
-
-  /*
-   * Swipe detect function from https://codepen.io/ganmahmud/pen/RaoKZa.
-   * @param {DOMElement} - el
-   * @param {Function} - callback
-   * @return {void}
-   */
-  function swipeDetect(el, callback) {
-    var touchsurface = el,
-        swipedir,
-        startX,
-        startY,
-        distX,
-        distY,
-        threshold = 150,
-        restraint = 100,
-        allowedTime = 300,
-        elapsedTime,
-        startTime,
-        handleswipe = callback || function(swipedir) { }
-
-    touchsurface.addEventListener('touchstart', function(e) {
-      var touchobj = e.changedTouches[0];
-      swipedir = 'none';
-      dist = 0;
-      startX = touchobj.pageX;
-      startY = touchobj.pageY;
-      startTime = new Date().getTime();
-    }, false);
-
-    touchsurface.addEventListener('touchmove', function(e) {
-      e.preventDefault();
-    }, false);
-
-    touchsurface.addEventListener('touchend', function(e) {
-      var touchobj = e.changedTouches[0];
-      distX = touchobj.pageX - startX;
-      distY = touchobj.pageY - startY;
-      elapsedTime = new Date().getTime() - startTime;
-      if (elapsedTime <= allowedTime) {
-        if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
-          swipedir = (distX < 0) ? 'left' : 'right';
-        } else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) {
-          swipedir = (distY < 0) ? 'up' : 'down';
-        }
-      }
-      handleswipe(swipedir);
-    }, false);
   }
 })();
